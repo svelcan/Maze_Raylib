b69dd746005f3b0ff1c699517f31a8777918dd6b
svelcan <svelcan@gmail.com>
2025-05-24 22:09:20 -0600

    Now the game runs out of the box

--- Source/build.bash
+++ Source/build.bash
@@ -1,3 +1,3 @@
-rm a.out
-g++ main.cpp core/Game.cpp core/Constants.cpp objects/Cell.cpp objects/CellGrid.cpp objects/Player.cpp states/Menu.cpp objects/ClickableObject.cpp objects/Flare.cpp -lraylib -lGL -lm -lpthread -ldl -lrt -lX11
-./a.out
+rm -f InvisibleMaze
+g++ main.cpp core/Game.cpp core/Constants.cpp objects/Cell.cpp objects/CellGrid.cpp objects/Player.cpp states/Menu.cpp states/ClickableObject.cpp objects/Flare.cpp -o InvisibleMaze -lraylib -lGL -lm -lpthread -ldl -lrt -lX11 -Wl,-rpath='$ORIGIN'
+./InvisibleMaze
